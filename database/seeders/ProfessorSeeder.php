7571885855c25be83f61f9ad94e7722ef4cc2ff8
kthitirat <kthitirat@aru.ac.th>
2024-07-24 14:45:10 +0700

    feat: add professor using mock

--- database/seeders/ProfessorSeeder.php
+++ database/seeders/ProfessorSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Professor;
+use App\Models\Department;
 
 class ProfessorSeeder extends Seeder
 {
@@ -13,6 +14,13 @@ class ProfessorSeeder extends Seeder
      */
     public function run(): void
     {
+        professor::factory()->create([
+            'prefix' => 'ดร.',
+            'first_name' => 'มรุต',
+            'last_name' => 'กลัดเจริญ',
+            'department_id' => Department::first()->id ?? Department::factory()
+        
+        ]);
         professor::factory()->count(10)->create();
     }
 }
