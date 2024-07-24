75ddac7ba836bde29744916ff9bb01cf3a4b3c59
kthitirat <kthitirat@aru.ac.th>
2024-07-24 15:44:36 +0700

    feat: add mock index page

--- app/Http/Controllers/PageController.php
+++ app/Http/Controllers/PageController.php
@@ -7,11 +7,14 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Inertia\Inertia;
 use App\Models\User;
+use App\Models\Subject;
 
 class PageController extends Controller
 {
     public function index()
     {
+        //$subjects = Subject::all();
+        
         return Inertia::render('Index');
     }
 
--- resources/js/Pages/Index.vue
+++ resources/js/Pages/Index.vue
@@ -1,7 +1,27 @@
 <template>
     <Layout>
-        <div>
-            dfdfdfdf
+        <div class="w-full">
+            <div class="flex justify-center">
+                <h1 class="text-4xl font-bold text-gray-700">เอกสารประกอบการสอน </h1>
+                <h1 class="text-4xl font-bold text-blue-600">(PowerPoint)</h1>
+            </div>
+            <div class="grid grid-cols-4 gap-8 px-16 mt-10">
+                <div class="w-full border border-slate-200 p-4">
+                    <img class="w-full h-80 object-cover" src="https://picsum.photos/400/800">
+                    <!-- <img class="w-full h-[500px] object-cover" src="https://picsum.photos/400/800"> -->
+                     <div class="mt-2">
+                        <p class="text-lg text-gray-700 text-center leading-5">รายวิชา 1161207 การอ่านคิดพัฒนาชีวิต</p>
+                        <div class="text-sm text-center mt-4">
+                            <p>อาจารย์รุ่งทิวา สุภานันท์</p>
+                            <p>คณะครุศาสตร์</p>
+                            <p>เผยแพร่วันที่ 10 มิถุนายน 2567</p>
+                            <p>จำนวนผู้เยี่ยมชม: </p>
+                        </div>
+                        
+                     </div>
+                </div>
+                         
+            </div>
         </div>
     </Layout>
 </template>
